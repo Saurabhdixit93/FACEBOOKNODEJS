2a624dd30a0dc91c486603ea4b5d0d0e76eadd41
Saurabh Dixit <smartds2550@gmail.com>
2023-05-21 19:29:28 +0000

    Update nodemailer.js

--- configration/nodemailer.js
+++ configration/nodemailer.js
@@ -1,23 +1,12 @@
 const nodemailer = require('nodemailer');
 
-// module.exports.transporter = nodemailer.createTransport({
-//     host: process.env.NODEMAILER_HOST,
-//     port: process.env.MAIL_PORT, 
-//     auth: {
-//       user:process.env.NODEMAILER_USERNAME,
-//       pass:process.env.NODEMAILER_PASSWORD
-//     },
-//     secure: true,
-// });
-
-
-// for testing mod
-
 module.exports.transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    host: process.env.NODEMAILER_HOST,
+    port: process.env.MAIL_PORT, 
     auth: {
-        user: 'emerald.durgan38@ethereal.email',
-        pass: 'DSvHBBnD932grU6qkK'
-    }
+      user:process.env.NODEMAILER_USERNAME,
+      pass:process.env.NODEMAILER_PASSWORD
+    },
+    secure: true,
 });
+
