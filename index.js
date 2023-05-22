95ab8edc40165f8b87f5c7bbd3386ed8ba9074ed
Saurabh Dixit <smartds2550@gmail.com>
2023-05-22 04:51:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');// importing express 
 const mongoose = require('mongoose');
-
 const cookieParser = require('cookie-parser'); // importing cookie-parser
 const dotenv = require('dotenv');
 dotenv.config();
@@ -22,7 +21,8 @@ const passportLocal = require('./configration/passport_local');// local passport
 const passportgoogle= require('./configration/passport_google_auth');// googl auth configration
 const passport_JWT = require('passport-jwt');// Requiring passport-jwt
 const passport_JWT_Stretgy = require('./configration/passport_jwt');
-const MongoStore = require('connect-mongo');// permanent store cookie in storemongo using connect-mongo
+const MongoStore = require('connect-mongo');// permanent store cookie in storemongo using connect-mong
+const nodemailer = require('nodemailer');
 app.use(express.static('assets')); //static files uses
 app.use('/uploads',express.static(__dirname + '/uploads')); // uploding img files
 app.use(expresslayouts);//layouts change
@@ -56,12 +56,31 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
-const nodemailer = require('nodemailer');
+app.use((err, req, res, next) => {
+    // Mongoose error handling
+    if (err.name === 'MongoError' || err.name === 'MongoNetworkError' || err.name === 'MongoServerSelectionError' || err.name === 'connect ETIMEDOUT' ) {
+      // Handle Mongoose errors and render 404 page
+      return res.render('404',{
+        title: '404 Page Not Found'
+      }); //  '404.ejs' file for the error page
+    } else if(err.name === 'Error' || err.name === 'getaddrinfo') {
+        return res.render('404',{
+            title: '404 Page Not Found'
+        });
+    }else {
+      // Pass the error to the next middleware in the stack
+      next(err);
+    };
+});
 
 app.use(flash()); // Using Flash 
 app.use(customFlashMiddleWare.setFlash); // using custum flash 
 app.use('/',require('./routers')); //Using Express Router For routing all access
-
+app.get('*', (req ,res) => {
+    return res.render('404',{
+        title: '404 Page Not Found'
+    });
+});
 // for production mode
 ConnectDB.CONNECTDB(). then(() => {
     app.listen(PORT, () =>{
